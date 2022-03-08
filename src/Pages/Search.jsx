e9f7f01d8c237c29c7ea319a51591714b74b91ed
Hanna Bacelar <hannabacelar@hotmail.com>
2022-03-08 18:43:15 -0300

    requisito 5

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import Header from './Header';
 
+const INITIAL_STATE = {
+  name: '',
+  procurar: true,
+};
+
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = INITIAL_STATE;
+  }
+
+  verificaBotao = (event) => {
+    const number2 = 2;
+
+    this.setState(() => ({
+      // name: event.target.value,
+      procurar: event.target.value.length < number2,
+
+    }));
+  }
+
   render() {
+    const { procurar } = this.state;
     return (
       <div data-testid="page-search">
         Search
         <Header />
+        <form>
+          <label htmlFor="cantor-input">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="name"
+              id="cantor-input"
+              onChange={ this.verificaBotao }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            name="procurar"
+            id="procurar"
+            disabled={ procurar }
+          >
+            Procurar
+          </button>
+        </form>
       </div>
     );
   }
